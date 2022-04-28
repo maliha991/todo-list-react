426b1123f047e965b7eb0985fdf200d10d375d12
maliha991 <maliha.mithila24@gmail.com>
2022-04-29 03:08:12 +0600

    Add: edit and delete item

--- src/App.js
+++ src/App.js
@@ -1,25 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import {
+	Box,
+	Flex,
+	Input,
+	InputGroup,
+	InputRightElement,
+	Text,
+	Center,
+} from "@chakra-ui/react";
+import { GoPlus } from "react-icons/go";
+import { ImCross } from "react-icons/im";
+
+import Item from "./component/Item";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [inputItem, setInputItem] = useState("");
+	const [todo, setTodo] = useState([]);
+	const [editingIndex, setEditingIndex] = useState(null);
+
+	const inputItemRef = useRef();
+
+	const cancelEditingHandler = () => {
+		setInputItem("");
+		setEditingIndex(null);
+	};
+
+	const addItemHandler = () => {
+		if (editingIndex !== null) {
+			const tempTodo = todo.reduce((result, item, index) => {
+				if (index === editingIndex) item.item = inputItem;
+				return [...result, item];
+			}, []);
+			setTodo(tempTodo);
+			setEditingIndex(null);
+		} else {
+			setTodo((prevState) => [...prevState, { item: inputItem, done: false }]);
+		}
+		setInputItem("");
+	};
+
+	const changeStatusHandler = (targetIndex) => {
+		const tempTodo = todo.reduce((result, item, index) => {
+			if (index === targetIndex) item.done = !item.done;
+			return [...result, item];
+		}, []);
+		setTodo(tempTodo);
+	};
+
+	const editItemHandler = (item, index) => {
+		setInputItem(item);
+		inputItemRef.current.focus();
+		setEditingIndex(index);
+	};
+
+	const deleteItemHandler = (item) => {
+		const tempTodo = todo.filter((currentItem) => currentItem.item !== item);
+		setTodo(tempTodo);
+	};
+
+	return (
+		<Flex minH="100vh" direction="column" alignItems="center">
+			<Text
+				m={20}
+				fontSize="5xl"
+				bgGradient="linear(to-l, #7928CA, #FF0080)"
+				bgClip="text"
+				fontWeight="bold"
+			>
+				To-do List
+			</Text>
+
+			<InputGroup w="20%" size="lg" mb={10}>
+				<Input
+					borderColor="#918EDC"
+					pr="4.5rem"
+					value={inputItem}
+					type="text"
+					placeholder="Track your todos"
+					onChange={(e) => setInputItem(e.target.value)}
+					ref={inputItemRef}
+					_focus={{ border: "2px solid #918EDC" }}
+				/>
+
+				<InputRightElement width={inputItem ? "5rem" : "3.5rem"}>
+					{inputItem && (
+						<Box
+							as="button"
+							bgGradient="linear(to-l, #7928CA, #FF0080)"
+							color="white"
+							w="1.5rem"
+							h="1.5rem"
+							borderRadius="20%"
+							cursor="pointer"
+							onClick={cancelEditingHandler}
+							mr={2}
+						>
+							<Center>
+								<ImCross fontSize="0.9rem" />
+							</Center>
+						</Box>
+					)}
+					<Box
+						as="button"
+						w="1.5rem"
+						borderRadius="20%"
+						bgGradient="linear(to-l, #7928CA, #FF0080)"
+						h="1.5rem"
+						onClick={addItemHandler}
+						color="white"
+						disabled={!inputItem}
+					>
+						<Center>
+							<GoPlus fontSize="1.2rem" />
+						</Center>
+					</Box>
+				</InputRightElement>
+			</InputGroup>
+
+			{todo.length > 0 &&
+				todo.map(({ item, done }, index) => (
+					<Item
+						item={item}
+						index={index}
+						done={done}
+						changeStatusHandler={changeStatusHandler}
+						editItemHandler={editItemHandler}
+						deleteItemHandler={deleteItemHandler}
+						key={++index}
+					/>
+				))}
+		</Flex>
+	);
 }
 
 export default App;
---
+++ src/component/Item.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Box, HStack, Icon, Spacer, Text } from "@chakra-ui/react";
+import { ImCross, ImCheckmark, ImClipboard } from "react-icons/im";
+import { AiOutlineFileExclamation, AiFillDelete } from "react-icons/ai";
+import { RiEdit2Fill } from "react-icons/ri";
+
+const Item = ({
+	item,
+	index,
+	done,
+	changeStatusHandler,
+	editItemHandler,
+	deleteItemHandler,
+}) => {
+	return (
+		<Box
+			borderRadius="500px"
+			w="30%"
+			m={2}
+			py={3}
+			px={5}
+			bgColor={done ? "rgba(121, 40, 202, 0.25)" : "rgba(255, 0, 128, 0.25)"}
+		>
+			<HStack>
+				<Icon
+					as={done ? ImClipboard : AiOutlineFileExclamation}
+					color={done ? "#7928CA" : "#FF0080"}
+					fontSize="xl"
+				/>
+				<Text textDecoration={done && "line-through"}>{item}</Text>
+				<Spacer />
+
+				<Box
+					as={!done ? ImCheckmark : ImCross}
+					color={!done ? "#7928CA" : "#FF0080"}
+					cursor="pointer"
+					onClick={() => changeStatusHandler(index)}
+				/>
+
+				<Box
+					as={RiEdit2Fill}
+					color="#413CAB"
+					cursor="pointer"
+					onClick={() => editItemHandler(item, index)}
+					fontSize="xl"
+				/>
+
+				<Box
+					as={AiFillDelete}
+					color="#FF2C2C"
+					cursor="pointer"
+					onClick={() => deleteItemHandler(item)}
+					fontSize="xl"
+				/>
+			</HStack>
+		</Box>
+	);
+};
+
+export default Item;
